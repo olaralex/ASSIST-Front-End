17ced98da593752a79f27536f798b073f7afa51b
Olar Alex <alexito.olar@gmail.com>
2023-01-02 20:19:52 +0200

    First interaction with JS

---
+++ Proiect JS/js/sandbox.js
@@ -0,0 +1,36 @@
+// matrix
+
+const line  = [1, 0, 1, 0];
+
+for(let i = 0; i < line.length; i++)
+{
+    console.log(line);
+}
+
+// switchs
+
+const favFood = 'pasta';
+
+switch(favFood)
+{
+    case 'pasta':
+        console.log('You have the best taste in Italian foods!');
+        break;
+    case 'tochitura':
+        console.log('You have the best taste in Bucovinian foods!');
+        break;
+    case 'mici':
+        console.log('You have the best taste in Romanian foods!');
+        break;
+    default:
+        console.log('You DONT have taste in foods!');
+}
+
+// matrix 2
+
+const matrix = [[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]]
+
+for(let i = 0; i<matrix.length; i++)
+{
+    console.log(matrix[i])
+}
