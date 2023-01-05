0aa909e135ad94f26b236113447f2c101406be01
Olar Alex <alexito.olar@gmail.com>
2023-01-05 16:52:29 +0200

    Learn objects and relative values

--- Proiect JS/js/sandbox.js
+++ Proiect JS/js/sandbox.js
@@ -37,98 +37,150 @@
 
 // Functions
 
-function hello(){
-    console.log('hello');
-}
+// function hello(){
+//     console.log('hello');
+// }
 
-hello();
+// hello();
 
 // Function Expression
 
-const hi = function(){
-    console.log('hi');
-};
+// const hi = function(){
+//     console.log('hi');
+// };
 
-hi();
+// hi();
 
 // Introducing Parameters To Functions
 
-const sayHelloTo = function(name = 'User' /* Default Parameter */){
-    console.log(`hello ${name}`);
-};
+// const sayHelloTo = function(name = 'User' /* Default Parameter */){
+//     console.log(`hello ${name}`);
+// };
 
-sayHelloTo('Alex');
+// sayHelloTo('Alex');
 
-// Returning Values
+// // Returning Values
 
-const whoIsBigger = function(a = NaN, b = NaN){
-    if(a<b){
-        return b;
-    }
-    else if(a<b){
-        return a;
-    }
-    else{
-        return;
-    }
-};
+// const whoIsBigger = function(a = NaN, b = NaN){
+//     if(a<b){
+//         return b;
+//     }
+//     else if(a<b){
+//         return a;
+//     }
+//     else{
+//         return;
+//     }
+// };
 
-let a = whoIsBigger(1, 2);
-console.log(a);
+// let a = whoIsBigger(1, 2);
+// console.log(a);
 
-let b = whoIsBigger();
-console.log(b);
+// let b = whoIsBigger();
+// console.log(b);
 
-const compareNumbers = function(x = NaN, y = NaN){
-    if(x>y){
-        console.log(`${x} is greater than ${y}!`);
-    }
-    else if(x<y){
-        console.log(`${y} is greater than ${x}!`);
-    }
-    else if(x===y){
-        console.log(`${x} and ${y} are equal!`);
-    }
-    else{
-        console.log('Please insert a number!');
-    }
-};
+// const compareNumbers = function(x = NaN, y = NaN){
+//     if(x>y){
+//         console.log(`${x} is greater than ${y}!`);
+//     }
+//     else if(x<y){
+//         console.log(`${y} is greater than ${x}!`);
+//     }
+//     else if(x===y){
+//         console.log(`${x} and ${y} are equal!`);
+//     }
+//     else{
+//         console.log('Please insert a number!');
+//     }
+// };
 
-compareNumbers(1, 2);
+// compareNumbers(1, 2);
 
-compareNumbers();
+// compareNumbers();
 
 // Arrow Funtions
 
-const calcPerimeter = (L,l) => {
-    return L * l;
-};
+// const calcPerimeter = (L,l) => {
+//     return L * l;
+// };
 
-const c = calcPerimeter(4,3);
-console.log(c);
+// const c = calcPerimeter(4,3);
+// console.log(c);
 
 // Callbacks And Foreach
 
-const fruits = ['apple', 'grapes', 'oranges', 'pears'];
+// const fruits = ['apple', 'grapes', 'oranges', 'pears'];
 
 // fruits.forEach(function(fruit){
 //     console.log(fruit);
 // });
 
-const indexOf = ((fruit, index) => console.log(`Index of ${fruit} is ${index}`));
+// const indexOf = ((fruit, index) => console.log(`Index of ${fruit} is ${index}`));
 
-fruits.forEach(indexOf); // Arrow the function 
+// fruits.forEach(indexOf); // Arrow the function 
 
 // Put datas into the browser
 
-const ul = document.querySelector('.numbers');
+// const ul = document.querySelector('.numbers');
 
-const numbers = [1, 2, 3, 4, 5, 6, 7];
+// const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-let html = ``;
+// let html = ``;
 
-numbers.forEach(number => html += `<li>${number}</li>`);
+// numbers.forEach(number => html += `<li>${number}</li>`);
 
-console.log(html);
+// console.log(html);
 
-ul.innerHTML = html;
+// ul.innerHTML = html;
+
+// Objescts
+
+let car = {
+    // literal
+    combustion: 'gas',
+    fabYear: 2014,
+    brand: 'Mercedes-Benz',
+    color: 'white',
+    specs: [
+        {specName: '3.0L engine', cost: '$$$'},
+        {specName: 'xenon lights', cost: '$'},
+        {specName: '4x4', cost: '$$'}
+    ],
+    // methods
+    turnOn: function() {
+        console.log('the car has turned ON!');
+    },
+    turnOff: function() {
+        console.log('the car has turned OFF!');
+    },
+    showSpecs: function() {
+        console.log('Specs of the car and the price are:')
+        this.specs.forEach(function(spec){
+            console.log(spec.specName + ', ' + spec.cost);
+        });
+    }
+};
+
+console.log(car);
+
+car.turnOn();
+
+car.showSpecs();
+
+car.turnOff();
+
+// Relative values
+
+person = {
+    age: 18,
+    nationality: 'Romanian'
+}
+
+const personOne = person;
+const personTwo = personOne;
+
+console.log(personOne, personTwo);
+
+personOne.age = 20;
+
+console.log(personOne, personTwo);
